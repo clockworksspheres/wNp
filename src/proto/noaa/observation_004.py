c40fe6bcb97c0c4adcb5e561005a4faa941e655b
Roy <roynielsen17@gmail.com>
2025-10-11 20:04:19 -0500

    simple class

--- src/proto/noaa/observation_004.py
+++ src/proto/noaa/observation_004.py
@@ -3,83 +3,66 @@
 import json
 from noaa_sdk import NOAA
 
-pzip = '39503'
+pzip = '83221'
 country = 'US'
 
-n = NOAA()
-observation = {}
-observations = n.get_observations(pzip, country)
-for item in observations:
-    observation = item
-    print(json.dumps(observation, indent=4))
-    break
+class noaa_job():
+    def __init__(self):
+        self.n = NOAA()
 
-relativeHumidity = {}
-barometricPressure = {}
-dewpoint = {}
-temperature = {}
+    def singleshot(self, pzip, country='US'):
+        observation = {}
+        observations = self.n.get_observations(pzip, country)
+        for item in observations:
+            observation = item
+            print(json.dumps(observation, indent=4))
+            break
+        return observation
 
-items = [relativeHumidity, barometricPressure, dewpoint, temperature]
+    def basic_vals(self, pzip, country):
+        
+        observation = self.singleshot(pzip, country)    
 
-for key, value in observation.items():
-    if key == "relativeHumidity":
-        relativeHumidity = value
-    elif key == "barometricPressure":
-        barometricPressure = value
-    elif key == "dewpoint":
-        dewpoint = value
-    elif key == "temperature":
-        temperature = value
-    else:
-        continue
+        relativeHumidity = {}
+        barometricPressure = {}
+        dewpoint = {}
+        temperature = {}
 
-print(f"temperature: {temperature['value']}")
-print(f"barometric pressure: {barometricPressure['value']}")
-print(f"relative humidity: {relativeHumidity['value']}")
-print(f"dewpoint: {dewpoint['value']}")
+        items = [relativeHumidity, barometricPressure, dewpoint, temperature]
 
-
-zip = '83221'
-
-n = NOAA()
-observation = {}
-observations = n.get_observations(pzip, country)
-for item in observations:
-    observation = item
-    print(json.dumps(observation, indent=4))
-    break
-
-relativeHumidity = {}
-barometricPressure = {}
-dewpoint = {}
-temperature = {}
-
-for key, value in observation.items():
-    if key == "relativeHumidity":
-        relativeHumidity = value
-    elif key == "barometricPressure":
-        barometricPressure = value
-        if barometricPressure:
-            barometricPressure['value'] = str(float(barometricPressure['value'])/100)
-        else:
-            barometricPressure = None
-        print(json.dumps(value, indent=4))
-    elif key == "dewpoint":
-        dewpoint = value
-    elif key == "temperature":
-        temperature = value
-        if temperature:
-            temperature['value'] = str(round(int(temperature['value'])*9/5+32))
-        else:
-            temperature = None
-    else:
-        continue
+        for key, value in observation.items():
+            if key == "relativeHumidity":
+                relativeHumidity = value
+            elif key == "barometricPressure":
+                barometricPressure = value
+                if barometricPressure:
+                    barometricPressure['value'] = str(float(barometricPressure['value'])/100)
+                else:
+                    barometricPressure = None
+                print(json.dumps(value, indent=4))
+            elif key == "dewpoint":
+                dewpoint = value
+            elif key == "temperature":
+                temperature = value
+                if temperature:
+                    temperature['value'] = str(round(int(temperature['value'])*9/5+32))
+                else:
+                    temperature = None
+            else:
+                continue
 
 
-print(f"temperature: {temperature['value']}")
-print(f"barometric pressure: {float(barometricPressure['value']):.5f}")
-print(f"relative humidity: {relativeHumidity['value']}")
-print(f"dewpoint: {dewpoint['value']}")
+        print(f"temperature: {temperature['value']}")
+        print(f"barometric pressure: {float(barometricPressure['value']):.5f}")
+        print(f"relative humidity: {relativeHumidity['value']}")
+        print(f"dewpoint: {dewpoint['value']}")
+
+
+njob = noaa_job()
+
+njob.basic_vals('39503', 'US')
+
+
 
     
 '''
