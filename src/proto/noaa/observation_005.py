9451e7e4b304760db1fd6021090a4c75d00ee251
Roy <roynielsen17@gmail.com>
2025-10-12 05:37:02 -0500

    slight mod

--- src/proto/noaa/observation_005.py
+++ src/proto/noaa/observation_005.py
@@ -65,7 +65,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A simple script to demonstrate argparse.")
     parser.add_argument("-z", "--zipcode", default="00000", help="zipcode to gather data on")
     parser.add_argument("-c", "--country", default='US', help="country to gather data on")
-
     args = parser.parse_args()
 
     njob = noaa_job()
