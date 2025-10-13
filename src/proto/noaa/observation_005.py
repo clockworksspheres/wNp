4261fef2aa0c636759c1e74a59faa4ade5e75dd1
Roy <roynielsen17@gmail.com>
2025-10-13 12:51:50 -0500

    error handling plus

--- src/proto/noaa/observation_005.py
+++ src/proto/noaa/observation_005.py
@@ -3,6 +3,8 @@
 import json
 from noaa_sdk import NOAA
 
+import traceback
+
 pzip = '83221'
 country = 'US'
 
@@ -32,28 +34,54 @@ class noaa_job():
 
         for key, value in observation.items():
             if key == "relativeHumidity":
-                relativeHumidity = value
+                try:
+                    relativeHumidity = value
+                except TypeError as err:
+                    if printErr:
+                        print(traceback.format_exc())
+                        print(str(err))
             elif key == "barometricPressure":
-                barometricPressure = value
-                if barometricPressure:
-                    barometricPressure['value'] = str(float(barometricPressure['value'])/100)
-                else:
-                    barometricPressure = None
-                # print(json.dumps(value, indent=4))
+                try:
+                    barometricPressure = value
+                    if barometricPressure:
+                        barometricPressure['value'] = str(float(barometricPressure['value'])/100)
+                    else:
+                        barometricPressure = None
+                        # print(json.dumps(value, indent=4))
+                except TypeError as err:
+                    if printErr:
+                        print(traceback.format_exc())
+                        print(str(err))
             elif key == "dewpoint":
-                dewpoint = value
+                try:
+                    dewpoint = value
+                except TypeError as err:
+                    if printErr:
+                        print(traceback.format_exc())
+                        print(str(err))
             elif key == "temperature":
-                temperature = value
-                if temperature:
-                    temperature['value'] = str(round(int(temperature['value'])*9/5+32))
-                else:
-                    temperature = None
+                try:
+                    temperature = value
+                    if temperature:
+                        temperature['value'] = str(round(int(temperature['value'])*9/5+32))
+                    else:
+                        temperature = None
+                except TypeError as err:
+                    if printErr:
+                        print(traceback.format_exc())
+                        print(str(err))
             else:
                 continue
 
 
         print(f"temperature: {temperature['value']}")
-        print(f"barometric pressure: {float(barometricPressure['value']):.5f}")
+        try:
+            print(f"barometric pressure: {float(barometricPressure['value']):.5f}")
+        except TypeErr as err:
+            if printErr as err:
+                print(traceback.format_exc())
+                print(str(err))
+        
         print(f"relative humidity: {relativeHumidity['value']}")
         print(f"dewpoint: {dewpoint['value']}")
 
