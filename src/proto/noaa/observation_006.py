d2fd512f7b6dc363215d017d2b4cadfeae247427
Roy <roynielsen17@gmail.com>
2025-10-13 20:09:38 -0500

    gave default to zip

--- src/proto/noaa/observation_006.py
+++ src/proto/noaa/observation_006.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="A simple script to demonstrate argparse.")
-    parser.add_argument("-z", "--zipcode", default="00000", help="zipcode to gather data on")
+    parser.add_argument("-z", "--zipcode", default="83221", help="zipcode to gather data on")
     parser.add_argument("-c", "--country", default='US', help="country to gather data on")
     args = parser.parse_args()
 
