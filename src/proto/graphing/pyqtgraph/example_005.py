2de3e0fe46a454f0a58dab2a715892de3930a551
Roy <roynielsen17@gmail.com>
2025-10-19 21:14:05 -0500

    converting to use of NoaaObservationRun

--- src/proto/graphing/pyqtgraph/example_005.py
+++ src/proto/graphing/pyqtgraph/example_005.py
@@ -14,125 +14,10 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 import pyqtgraph as pg
 
 #####
-# NOAA library to access weather data
-# pip install noaa_sdk
-from noaa_sdk import NOAA
-
-
-class noaa_job():
-    def __init__(self):
-        self.n = NOAA()
-
-    def singleshot(self, pzip, country='US'):
-        observation = {}
-        observations = self.n.get_observations(pzip, country)
-        for item in observations:
-            observation = item
-            # :print(json.dumps(observation, indent=4))
-            break
-        return observation
-
-    def fordays(self, pzip, country='US'):
-        i = 1
-        observation = OrderedDict()
-        observations = self.n.get_observations(pzip, country)
-        for item in observations:
-            tmp = {i : item}
-            observation.update(tmp)
-            # print(json.dumps(observation, indent=4))
-            #break
-            i += 1
-        # print(json.dumps(observation, indent=4))
-        return observation
-
-
-    def basic_vals(self, pzip, country):
-        
-        observation = self.singleshot(pzip, country)    
-
-        relativeHumidity = {}
-        barometricPressure = {}
-        dewpoint = {}
-        temperature = {}
-
-        items = [relativeHumidity, barometricPressure, dewpoint, temperature]
-
-        for key, value in observation.items():
-            if key == "relativeHumidity":
-                relativeHumidity = value
-            elif key == "barometricPressure":
-                barometricPressure = value
-                if barometricPressure:
-                    barometricPressure['value'] = str(float(barometricPressure['value'])/100)
-                else:
-                    barometricPressure = None
-                # print(json.dumps(value, indent=4))
-            elif key == "dewpoint":
-                dewpoint = value
-            elif key == "temperature":
-                temperature = value
-                if temperature:
-                    temperature['value'] = str(round(int(temperature['value'])*9/5+32))
-                else:
-                    temperature = None
-            else:
-                continue
-
-        # print(f"temperature: {temperature['value']}")
-        # print(f"barometric pressure: {float(barometricPressure['value']):.5f}")
-        # print(f"relative humidity: {relativeHumidity['value']}")
-        # print(f"dewpoint: {dewpoint['value']}")
-
-    def fordays_vals(self, pzip, country):
-        
-        observation = self.fordays(pzip, country)    
-
-        # print(str(observation))
-
-        # print(json.dumps(observation, indent=4))
-        # return
-
-        relativeHumidity = {}
-        barometricPressure = {}
-        dewpoint = {}
-        temperature = {}
-
-        items = [relativeHumidity, barometricPressure, dewpoint, temperature]
-
-        for keys, values in observation.items():
-            #print(values)
-            relativeHumidity = {}
-            barometricPressure = {}
-            dewpoint = {}
-            temperature = {}
-
-            for key, value in values.items():
-                # print(value)
-                if key == "relativeHumidity":
-                    relativeHumidity = value
-                elif key == "barometricPressure":
-                    barometricPressure = value
-                    if barometricPressure:
-                        barometricPressure['value'] = str(float(barometricPressure['value'])/100)
-                    else:
-                        barometricPressure = None
-                    # print(json.dumps(value, indent=4))
-                elif key == "dewpoint":
-                    dewpoint = value
-                elif key == "temperature":
-                    temperature = value
-                    if temperature:
-                        temperature['value'] = str(round(int(temperature['value'])*9/5+32))
-                    else:
-                        temperature = None
-                else:
-                    continue
-
-            # print(f"temperature: {temperature['value']}")
-            # print(f"barometric pressure: {float(barometricPressure['value']):.5f}")
-            # print(f"relative humidity: {relativeHumidity['value']}")
-            # print(f"dewpoint: {dewpoint['value']}")
-
+# load internal libraries
+sys.path.append("../../..")
+from wnp.lib.NoaaObservationRun import NoaaObservationRun
+ 
 
 class MainWindow(QMainWindow):
 
@@ -142,7 +27,7 @@ class MainWindow(QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
         
-        self.njob = noaa_job()
+        self.njob = NoaaObservationRun()
         '''
         temperature = [30,32,34,32,33,31,29,32,35,45]
 
--- src/proto/graphing/pyqtgraph/example_009.5.py
+++ src/proto/graphing/pyqtgraph/example_009.5.py
@@ -18,112 +18,11 @@ import pyqtgraph as pg
 # pip install noaa_sdk
 from noaa_sdk import NOAA
 
+#####
+# Load local libraries
 sys.path.append("../../..")
-
+from wnp.lib.NoaaObservationRun import NoaaObservationRun
 from wnp.config import DEFAULT_DEGREES_UNITS
-
-
-class noaa_job():
-    def __init__(self):
-        self.n = NOAA()
-
-    def singleshot(self, pzip, country='US'):
-        observation = {}
-        observations = self.n.get_observations(pzip, country)
-        stuff = {}
-        tags = ['timestamp', 'temperature', 'dewpoint', 'relativeHumidity', 'barometricPressure']
-        for observation in observations:
-            #print(json.dumps(observation, indent=4))
-            for keys, values in observation.items():
-                if keys in tags:
-                    # print(f"{keys}")
-                    if isinstance(values, dict):
-                        # print(f"{keys}: {values['value']}")
-                        stuff[keys] = values['value']
-                    elif isinstance(values, str) and keys == 'timestamp':
-                        print(f"{keys}: {values}")
-                        timestamp = values.split("T")[-1]
-                        hour = timestamp.split(":")[0]
-                        minute = timestamp.split(":")[1]
-                        if int(hour) == 0:
-                            hour = 24
-                        timestamp = f"{int(hour)-5}{minute}"
-                        stuff[keys] = timestamp
-            break
-
-        return stuff
-
-    def fordays(self, pzip, country='US', samples=10, timestamp=False):
-        observation = {}
-        observations = self.n.get_observations(pzip, country)
-        stuff = {}
-        tags = ['timestamp', 'temperature', 'dewpoint', 'relativeHumidity', 'barometricPressure']
-        i = 0
-        container = OrderedDict()
-        for observation in observations:
-            #print(json.dumps(observation, indent=4))
-            i += 1
-            
-            tmptimestamp = observation['timestamp'].split("T")[-1]
-            hour = tmptimestamp.split(":")[0]
-            minute = tmptimestamp.split(":")[1]
-            if int(hour) == 0:
-                hour = 24
-            tmpdate = observation['timestamp'].split("T")[0]
-            #tmpdate = int(''.join(tmpdate.split('-')))
-            tmptimestamp = f"{tmpdate}-{(int(hour)-5)%24}{minute}"
-
-            if timestamp:
-                container[tmptimestamp] = {}
-                container[tmptimestamp]['timestamp'] = tmptimestamp
-                if DEFAULT_DEGREES_UNITS == 'C' and observation['temperature']['unitCode'][-1] == 'F':
-                    container[tmptimestamp]['temperature'] = (float(observation['temperature']['value'])-32)*5/9
-                elif DEFAULT_DEGREES_UNITS == 'F' and observation['temperature']['unitCode'][-1] == 'C':
-                    container[tmptimestamp]['temperature'] = float(observation['temperature']['value'])*9/5 + 32
-                else:
-                    container[tmptimestamp]['temperature'] = float(observation['temperature']['value'])
-                try:
-                    if DEFAULT_DEGREES_UNITS == 'C' and observation['dewpoint']['unitCode'][-1] == 'F':
-                        container[tmptimestamp]['dewpoint'] = (float(observation['dewpoint']['value'])-32)*5/9
-                    elif DEFAULT_DEGREES_UNITS == 'F' and observation['dewpoint']['unitCode'][-1] =='C':
-                        container[tmptimestamp]['dewpoint'] = float(observation['dewpoint']['value'])*9/5 + 32
-                    else:
-                        container[tmptimestamp]['dewpoint'] = float(observation['dewpoint']['value'])
-                except TypeError as err:
-                    container[tmptimestamp]['dewpoint'] = 0
-                try:
-                    container[tmptimestamp]['relativeHumidity'] = float(observation['relativeHumidity']['value'])
-                except TypeError as err:
-                    container[tmptimestamp]['relativeHumidity'] = 0
-                container[tmptimestamp]['barometricPressure'] = float(observation['barometricPressure']['value'])
-            else:
-                container[i] = {}
-                container[i]['timestamp'] = tmptimestamp
-                if DEFAULT_DEGREES_UNITS == 'C' and observation['temperature']['unitCode'][-1] == 'F':
-                    container[i]['temperature'] = (float(observation['temperature']['value'])-32)*5/9
-                elif DEFAULT_DEGREES_UNITS == 'F' and observation['temperature']['unitCode'][-1] == 'C':
-                    container[i]['temperature'] = float(observation['temperature']['value'])*9/5 + 32
-                else:
-                    container[i]['temperature'] = float(observation['temperature']['value'])
-                try:
-                    if DEFAULT_DEGREES_UNITS == 'C' and observation['dewpoint']['unitCode'][-1] == 'F':
-                        container[i]['dewpoint'] = (float(observation['dewpoint']['value'])-32)*5/9
-                    elif DEFAULT_DEGREES_UNITS == 'F' and observation['dewpoint']['unitCode'][-1] =='C':
-                        container[i]['dewpoint'] = float(observation['dewpoint']['value'])*9/5 + 32
-                    else:
-                        container[i]['dewpoint'] = float(observation['dewpoint']['value'])
-                except TypeError as err:
-                    container[i]['dewpoint'] = 0
-                try:
-                    container[i]['relativeHumidity'] = float(observation['relativeHumidity']['value'])
-                except TypeError as err:
-                    container[i]['relativeHumidity'] = 0
-                container[i]['barometricPressure'] = float(observation['barometricPressure']['value'])
-            if int(i) == int(samples):
-                break
-        print(f"{json.dumps(container, indent=4)}")
-
-        return container
 
 
 class MainWindow(QMainWindow):
@@ -134,7 +33,7 @@ class MainWindow(QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
         
-        self.njob = noaa_job()
+        self.njob = NoaaObservationRun()
 
         self.lineColor = { 'temperature' : 'blue', 'dewpoint' : 'green', 'relativeHumidity' : 'purple', 'barometricPressure' : 'orange' }
 
