1127f9bbb92c6209f51dd940d37326b668661790
Roy <roynielsen17@gmail.com>
2026-01-10 19:06:20 -0700

    adjusting build scripts

--- src/BuildScripts/build.ubuntu2404.sh
+++ src/BuildScripts/build.ubuntu2404.sh
@@ -30,11 +30,11 @@ else
    source packenv/bin/activate
 fi
 
-#cp BuildScripts/build.ubuntu2024.py312.onefile.spec .
+cp BuildScripts/build.ubuntu2404.py312.onefile.spec .
 
-#pyinstaller --clean -y build.ubuntu2024.py312.onefile.spec
-#pyinstaller -y build.ubuntu2024.py312.onefile.spec
-#rm build.ubuntu2024.py312.onefile.spec
+pyinstaller --clean -y build.ubuntu2404.py312.onefile.spec
+pyinstaller -y build.ubuntu2404.py312.onefile.spec
+rm build.ubuntu2404.py312.onefile.spec
 
 popd
 
